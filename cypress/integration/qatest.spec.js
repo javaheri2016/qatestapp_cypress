3053b5fa3d42b32d934c6e6402cf120051f2da08
javaheri2016 <Marker22%>
2021-06-21 21:53:16 +0200

    cypress tests ready to run

---
+++ cypress/integration/qatest.spec.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+describe('Provide a correct command', () => {
+    it('should navigate to QATest app', () => {
+        cy.visit('https://qa-task.netlify.app/')    
+        cy.get('#mat-input-0').type("uname")
+        cy.get('.mat-button').click()
+        cy.get('pre').contains("Linux")
+    })
+})
+
+describe('Provide an incorrect command', () => {
+    it('should navigate to QATest app', () => {
+        cy.visit('https://qa-task.netlify.app/')    
+        cy.get('#mat-input-0').type("UNAME")
+        cy.get('.mat-button').click()
+        cy.get('pre').contains("error: command not supported.")
+    })
+})
+
+describe('Provide an empty command', () => {
+    it('should navigate to QATest app', () => {
+        cy.visit('https://qa-task.netlify.app/')    
+        cy.get('.mat-button').click()
+        cy.get('pre').contains("error: no command.")
+    })
+})
+
+describe('Provide an echo command', () => {
+    var com = "sample"
+    it('should navigate to QATest app', () => {
+        cy.visit('https://qa-task.netlify.app/') 
+        cy.get('#mat-input-0').type("echo " + com)   
+        cy.get('.mat-button').click()
+        cy.get('pre').contains(com)
+    })
+})
